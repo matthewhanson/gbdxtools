12a01070bd002d5bbf25f681bd4ad4a8ba89e039
chelm <christopner.helm@gmail.com>
2018-03-06 16:22:40 +0000

    doing a simple queue for multi curl

--- gbdxtools/ipe/fetch/async/libcurl/select.py
+++ gbdxtools/ipe/fetch/async/libcurl/select.py
@@ -27,73 +27,94 @@ except ImportError:
 else:
     signal.signal(SIGPIPE, SIG_IGN)
 
+NUM_CONN = int(os.environ.get('GBDX_THREADS', 64))
+print(NUM_CONN)
 NUM_WORKERS = 5
 MAX_RETRIES = 5
 
-def _setup_curl(url, token, index, NOSIGNAL=1, CONNECTTIMEOUT=120, TIMEOUT=300):
-    _, ext = os.path.splitext(urlparse(url).path)
-    fp = NamedTemporaryFile(prefix='gbdxtools', suffix=ext, delete=False)
-    _curl = pycurl.Curl()
-    _curl.setopt(pycurl.NOSIGNAL, NOSIGNAL)
-    _curl.setopt(pycurl.CONNECTTIMEOUT, CONNECTTIMEOUT)
-    _curl.setopt(pycurl.TIMEOUT, TIMEOUT)
-    _curl.setopt(pycurl.URL, url)
-    _curl.setopt(pycurl.HTTPHEADER, ['Authorization: Bearer {}'.format(token)])
-    _curl.setopt(pycurl.WRITEDATA, fp.file)
-    _curl.index = index
-    _curl.token = token
-    _curl.url = url
-    return (_curl, fp)
-
 def load_urls(collection, shape=(8,256,256), max_retries=MAX_RETRIES):
+    results = {}
+    queue = collection
     mc = pycurl.CurlMulti()
-    nhandles = len(collection)
-    results, cmap = {}, {}
-    for url, token, index in collection:
-        index = tuple(index)
-        _curl, fp = _setup_curl(url, token, index)
-        cmap[index] = (_curl, fp)
-        mc.add_handle(_curl)
+    mc.handles = []
 
-    nprocessed = 0
-    while nhandles - nprocessed:
-        mc.select(1.0)
-        while True:
-            ret, something = mc.perform()
+    num_urls = len(queue)
+    num_conn = min(NUM_CONN, num_urls)
+
+    for i in range(num_conn):
+        c = pycurl.Curl()
+        c.fp = None
+        c.setopt(pycurl.FOLLOWLOCATION, 1)
+        c.setopt(pycurl.MAXREDIRS, 5)
+        c.setopt(pycurl.CONNECTTIMEOUT, 120)
+        c.setopt(pycurl.TIMEOUT, 300)
+        c.setopt(pycurl.NOSIGNAL, 1)
+        mc.handles.append(c)
+
+    freelist = mc.handles[:]
+    num_processed = 0
+    while num_processed < num_urls:
+        while queue and freelist:
+            url, token, index = queue.pop(0)
+            c = freelist.pop()
+            _, ext = os.path.splitext(urlparse(url).path)
+            fp = NamedTemporaryFile(prefix='gbdxtools', suffix=ext, delete=False)
+            c.fp = fp 
+            c.setopt(pycurl.URL, url)
+            c.setopt(pycurl.HTTPHEADER, ['Authorization: Bearer {}'.format(token)])
+            c.setopt(pycurl.WRITEDATA, c.fp.file)
+            mc.add_handle(c)
+            # store some info
+            c.index = index
+            c.token = token
+            c.url = url
+
+        while 1:
+            ret, num_handles = mc.perform()
             if ret != pycurl.E_CALL_MULTI_PERFORM:
                 break
-        nq, suc, failed = mc.info_read()
-        nprocessed += len(suc)
-        #TODO: handler function
-        for h in suc:
-            _fp = cmap[h.index][-1]
-            _fp.flush()
-            _fp.close()
-            try:
-                arr = imread(_fp.name)
-                if len(arr.shape) == 3:
-                    arr = np.rollaxis(arr, 2, 0)
-                else:
-                    arr = np.expand_dims(arr, axis=0)
-            except Exception as e:
-                print(e)
-                arr = np.zeros(shape, dtype=np.float32)
-            finally:
-                results[h.index] = arr
-                h.close()
-                mc.remove_handle(h)
-                os.remove(_fp.name)
-        for h, err_num, err_msg in failed:
-            print('failed: {}, code={}, msg={}'.format(h.index, err_num, err_msg))
-            _fp = cmap[h.index][-1]
-            _fp.flush()
-            _fp.close()
-            os.remove(_fp.name)
-            h.close()
-            mc.remove_handle(h)
-            _curl, fp = _setup_curl(h.url, h.token, h.index)
-            cmap[h.index] = (_curl, fp)
-            mc.add_handle(_curl)
+
+        # Check for curl objects which have terminated, and add them to the freelist
+        while 1:
+            num_q, ok_list, err_list = mc.info_read()
+            for c in ok_list:
+                c.fp.flush()
+                c.fp.close()
+                try:
+                    arr = imread(c.fp.name)
+                    if len(arr.shape) == 3:
+                       arr = np.rollaxis(arr, 2, 0)
+                    else:
+                       arr = np.expand_dims(arr, axis=0)
+                except Exception as e:
+                    print(e)
+                    arr = np.zeros(shape, dtype=np.float32)
+                finally:
+                    results[c.index] = arr
+                    mc.remove_handle(c)
+                    os.remove(c.fp.name)
+                    c.fp = None
+                    freelist.append(c)
+                    #print("Success:", c.url, c.getinfo(pycurl.EFFECTIVE_URL))
+            for c, errno, errmsg in err_list:
+                _fp = cmap[h.index][-1]
+                c.fp.flush()
+                c.fp.close()
+                os.remove(c.fp.name)
+                mc.remove_handle(c)
+                c.fp = None
+                print("Failed: ", c.url, errno, errmsg)
+                freelist.append(c)
+            num_processed = num_processed + len(ok_list) + len(err_list)
+            if num_q == 0:
+                break
+        mc.select(1.0)
+
+    for c in mc.handles:
+        if c.fp is not None:
+            c.fp.close()
+            c.fp = None
+        c.close()
 
     mc.close()
     return results
